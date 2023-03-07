1c836d24ef9dddf9fd2294389f5ed6a4aecd332f
Jessica Miller <jessica.miller@tlgcohort.com>
2023-03-07 07:44:10 -0800

    Scratch module progress

--- Scratch/src/com/crunch/RadishSortTest.java
+++ Scratch/src/com/crunch/RadishSortTest.java
@@ -1,6 +1,7 @@
 package com.crunch;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 class RadishSortTest {
@@ -15,15 +16,39 @@ class RadishSortTest {
 
         dump(radishes);
         System.out.println("original list");
+        System.out.println();
 
+        System.out.println("sort by natural order (size)");
         radishes.sort(null); //passing null means natural order
         dump(radishes);
-        System.out.println("natural order");
+        System.out.println();
 
         //sort by color, via an instance of RadishColorComparator
         radishes.sort(new RadishColorComparator());
         dump(radishes);
         System.out.println("Comparator sort by color");
+        System.out.println();
+
+        //radishes sort by tail length ANONYMOUS CLASS DEFINITION
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                //return wrapper around getTailLength method
+                return Double.compare(r1.getTailLength(), r2.getTailLength());
+            }
+        });
+        dump(radishes); //YOU MUST DUMP IT
+        System.out.println();
+
+        System.out.println("sort by sprouts via anonymous class");
+        radishes.sort(new Comparator<Radish>() {
+            @Override
+            public int compare(Radish r1, Radish r2) {
+                return Integer.compare(r1.getSprouts(), r2.getSprouts());
+            }
+        });
+        dump(radishes);
+        System.out.println();
 
     }
     private static void dump(List<Radish> radishList) {
