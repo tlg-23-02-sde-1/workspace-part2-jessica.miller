ce09c20a93624d146c04cbfb4e8fdaa810db0f98
Jessica Miller <jessica.miller@tlgcohort.com>
2023-03-07 15:49:37 -0800

    Lab 8.1 progress

--- Lab08.1/test/com/javatunes/compare/StringSortTest.java
+++ Lab08.1/test/com/javatunes/compare/StringSortTest.java
@@ -33,5 +33,8 @@ public class StringSortTest {
     // TODO: call names.sort() and pass in an "expression" lambda
     // hint: a compatible lambda will have this form:
     //       (obj1, obj2) -> expression-that-evaluates-to-int
+    System.out.println("Lambda order:");
+    names.sort((name1, name2) -> Integer.compare(name1.length(), name2.length()));
+    System.out.println(names + "\n");
   }
 }
